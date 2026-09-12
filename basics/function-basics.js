33a82d8af15418d4fa19674d8d0bdbb2de7b7c0a
Denis Matern <denismatern@gmail.com>
2026-09-12 15:45:57 +0300

    feat: function pow

--- basics/function-basics.js
+++ basics/function-basics.js
@@ -1,20 +1,46 @@
 "use strict";
 
-function checkAge(age) {
-    return (age > 18) || confirm('Родители разрешили?');
-};
-checkAge(18);
-checkAge(21);
+// function checkAge(age) {
+//     return (age > 18) || confirm('Родители разрешили?');
+// };
+// checkAge(18);
+// checkAge(21);
 
-function min(a, b) {
-    return a < b ? a : b;
-};
-alert(min(2, 3));
-alert(min(4, 1));
+// function min(a, b) {
+//     return a < b ? a : b;
+// };
+// alert(min(2, 3));
+// alert(min(4, 1));
 
-function pow(x,n) {
-    if (n < 1) return;
-    return x ** n;
-};
-alert(pow(2, 3));
-alert(pow(4, 1));
+// function pow(x,n) {
+//     if (n < 1) return;
+//     return x ** n;
+// };
+// alert(pow(2, 3));
+// alert(pow(4, 1));
+
+// 1. Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
+// function min(a, b) {
+//     return a < b ? a : b;
+// }
+// console.log(min(2, 5));
+// console.log(min(3, -1));
+// console.log(min(1, 1));
+
+// 2. Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
+function pow(x, n) {
+    if (n < 1) return alert('Степень должна быть > 1');
+    
+    return x**n;
+}
+
+console.log(pow(3, 2));
+console.log(pow(3, 3));
+console.log(pow(1, 100));
+
+const x = prompt('Введите число возводимое в степень', 2);
+const n = prompt('Введите степень', 2);
+
+if (x > 0 && n > 1) {
+    alert(`prompt ${pow(x, n)}`);
+}
